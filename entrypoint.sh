120a24154e4306b973e27140fe627642f1acff54
Jabran Rafique <jabran.rafique@ratedpeople.com>
2022-10-09 08:29:30 +0100

    update dev setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
+# validate composer.json file
+echo -e "\n"
+echo "Validate composer.json"
+echo "========================"
+composer validate
+
 # install dependencies
+echo -e "\n"
+echo "Install dependencies"
+echo "========================"
 composer install -v --no-interaction --prefer-source
 
 # run unit tests
+echo -e "\n"
+echo "Run tests"
+echo "========================"
 composer test
